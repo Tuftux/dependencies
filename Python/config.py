7b61f9b4c7a71ca8a743497ea9184160e6a609ec
Tuft <alexandre@r5t.private>
2020-04-25 12:29:56 -0400

    Fixed copying of the python install files to the build directory.

--- Python/config.py
+++ Python/config.py
@@ -118,15 +118,14 @@
 		"commands": [
 			"xcopy /y /e ..\\..\\patches\\2.7.17",
 			"CALL PCbuild\\build.bat -e -m -v -p=x64 \"/p:PlatformToolset=v140\"",
-			"xcopy /y /E Include {buildDir}\\include\\"
+			"xcopy /y /E Include {buildDir}\\include\\",
 			"xcopy /y /E Lib {buildDir}\\Lib\\",
-			"cd PCbuild\\amd64",
-			"for /r %%a in (*.dll) do xcopy /y /q \"%%a\" {buildDir}\\DLLs\\ /i",
-			"for /r %%a in (*.pdb) do xcopy /y /q \"%%a\" {buildDir}\\DLLs\\ /i",
-			"for /r %%a in (*.pyd) do xcopy /y /q \"%%a\" {buildDir}\\DLLs\\ /i",
-			"for /r %%a in (*.lib) do xcopy /y /q \"%%a\" {buildDir}\\libs\\ /i",
-			"for /r %%a in (*.exe) do xcopy /y /q \"%%a\" {buildDir}\\ /i",
-			"copy python27.dll {buildDir}\\python27.dll",
+			"for /r PCBuild\\amd64 %a in (*.dll) do xcopy /y /q \"%a\" {buildDir}\\DLLs\\ /i",
+			"for /r PCBuild\\amd64 %a in (*.pdb) do xcopy /y /q \"%a\" {buildDir}\\DLLs\\ /i",
+			"for /r PCBuild\\amd64 %a in (*.pyd) do xcopy /y /q \"%a\" {buildDir}\\DLLs\\ /i",
+			"for /r PCBuild\\amd64 %a in (*.lib) do xcopy /y /q \"%a\" {buildDir}\\libs\\ /i",
+			"for /r PCBuild\\amd64 %a in (*.exe) do xcopy /y /q \"%a\" {buildDir}\\ /i",
+			"copy PCBuild\\amd64\\python27.dll {buildDir}\\python27.dll",
 			"copy PC\\pyconfig.h {buildDir}\\include\\pyconfig.h"
 		]
 	}
