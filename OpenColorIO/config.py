846cec83d3720fefc9db4027522f3dec9a891ce3
Tuft <alexandre@r5t.private>
2020-04-26 18:13:59 -0400

    OpenColorIO : Fixed building on Windows.
    Updated to 1.1.1 to get cmake fixes and match VFX reference platform.

--- OpenColorIO/config.py
+++ OpenColorIO/config.py
@@ -2,7 +2,7 @@
 
 	"downloads" : [
 
-		"https://github.com/imageworks/OpenColorIO/archive/v1.0.9.tar.gz",
+		"https://github.com/AcademySoftwareFoundation/OpenColorIO/archive/v1.1.1.tar.gz",
 		"https://github.com/imageworks/OpenColorIO-Configs/archive/v1.0_r2.tar.gz",
 
 	],
@@ -54,4 +54,29 @@
 
 	],
 
+	"platform:windows": {
+		"commands": [
+			"cmake -G \"Visual Studio 15 2017 Win64\""
+			" -DCMAKE_INSTALL_PREFIX={buildDirWindows}"
+			" -DCMAKE_PREFIX_PATH={buildDirWindows}"
+			" -DPython_LIBRARY={buildDirWindows}\\libs\\python27.lib"
+			" -DPython_INCLUDE_DIR={buildDirWindows}\\include"
+			" -DPython_EXECUTABLE={buildDirWindows}\\python.exe"
+			" -DOCIO_USE_BOOST_PTR=1"
+			" -DOCIO_BUILD_TRUELIGHT=OFF"
+			" -DOCIO_BUILD_APPS=OFF"
+			" -DOCIO_BUILD_NUKE=OFF"
+			" .",
+
+			"cmake --build . --config Release",
+			"cmake --build . --config Release --target INSTALL",
+
+			"if not exist {buildDirWindows}\\python md {buildDirWindows}\\python",
+			"move {buildDirWindows}\\lib\\site-packages\\PyOpenColorIO{pythonSharedLibraryExtension} {buildDirWindows}\\python\\",
+
+			"if not exist {buildDirWindows}\\openColorIO md {buildDirWindows}\\openColorIO",
+			"copy ..\\OpenColorIO-Configs-1.0_r2\\nuke-default\\config.ocio {buildDirWindows}\\openColorIO",
+			"copy ..\\OpenColorIO-Configs-1.0_r2\\nuke-default\\luts {buildDirWindows}\\openColorIO"
+		]
+	}
 }
