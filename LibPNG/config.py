e1f041713b02f8635b6f1774c9be60807ed7464d
Tuft <alexandre@r5t.private>
2020-04-26 17:39:30 -0400

    LibPNG : Fixed building on Windows.

--- LibPNG/config.py
+++ LibPNG/config.py
@@ -24,4 +24,15 @@
 
 	],
 
+	"platform:windows": {
+		"commands": [
+			"cmake -G \"Visual Studio 15 2017 Win64\""
+			" -DCMAKE_INSTALL_PREFIX={buildDirWindows}"
+			" -DCMAKE_PREFIX_PATH={buildDirWindows}"
+			" .",
+
+			"cmake --build . --config Release",
+			"cmake --build . --config Release --target INSTALL",
+		]
+	}
 }
